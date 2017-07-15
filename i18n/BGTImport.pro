77092e1571cb6e61f61d24a18157a81f5f99c2f8
deduikertjes <deduikertjes@xs4all.nl>
2017-07-15 13:21:27 +0200

    added dutch translation

---
+++ i18n/BGTImport.pro
@@ -0,0 +1,7 @@
+FORMS = ../bgt_import_dialog_base.ui
+
+SOURCES = ../bgt_import.py \
+../bgt_import.py
+
+TRANSLATIONS = BGTImport_nl.ts
+
---
+++ i18n/BGTImport_nl.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl_NL">
+<context>
+    <name>BGTImport</name>
+    <message>
+        <location filename="../bgt_import.py" line="205"/>
+        <source>&amp;BGT Import</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../bgt_import.py" line="195"/>
+        <source>BGT Import</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../bgt_import.py" line="260"/>
+        <source>Select BGT gml file(s)</source>
+        <translation>Selecteer BGT gml bestand(en)</translation>
+    </message>
+    <message>
+        <location filename="../bgt_import.py" line="293"/>
+        <source>Analyzing file </source>
+        <translation>Analyseren bestand </translation>
+    </message>
+    <message>
+        <location filename="../bgt_import.py" line="342"/>
+        <source> from </source>
+        <translation> van </translation>
+    </message>
+    <message>
+        <location filename="../bgt_import.py" line="298"/>
+        <source>Importing file </source>
+        <translation>Importeren bestand </translation>
+    </message>
+    <message>
+        <location filename="../bgt_import.py" line="316"/>
+        <source>Error in creating gml copies for import: </source>
+        <translation>Fout bij aanmaken gml kopien voor import: </translation>
+    </message>
+    <message>
+        <location filename="../bgt_import.py" line="330"/>
+        <source>Error in reading import definitions: </source>
+        <translation>Fout bij het lezen van import definities: </translation>
+    </message>
+    <message>
+        <location filename="../bgt_import.py" line="339"/>
+        <source>Error in writing import definitions: </source>
+        <translation>Fout bij het wegschrijven van import definities: </translation>
+    </message>
+    <message>
+        <location filename="../bgt_import.py" line="342"/>
+        <source>Adding file </source>
+        <translation>Toevoegen bestand </translation>
+    </message>
+</context>
+<context>
+    <name>BGTImportDialogBase</name>
+    <message>
+        <location filename="../bgt_import_dialog_base.ui" line="14"/>
+        <source>BGT Import</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../bgt_import_dialog_base.ui" line="42"/>
+        <source>Browse</source>
+        <translation>Bladeren</translation>
+    </message>
+    <message>
+        <location filename="../bgt_import_dialog_base.ui" line="77"/>
+        <source>Select BGT gml</source>
+        <translation>Selecteer BGT gml</translation>
+    </message>
+    <message>
+        <location filename="../bgt_import_dialog_base.ui" line="90"/>
+        <source>Polygons</source>
+        <translation>Vlakken</translation>
+    </message>
+    <message>
+        <location filename="../bgt_import_dialog_base.ui" line="103"/>
+        <source>Lines</source>
+        <translation>Lijnen</translation>
+    </message>
+    <message>
+        <location filename="../bgt_import_dialog_base.ui" line="116"/>
+        <source>Points</source>
+        <translation>Punten</translation>
+    </message>
+    <message>
+        <location filename="../bgt_import_dialog_base.ui" line="129"/>
+        <source>Import</source>
+        <translation>Importeer</translation>
+    </message>
+    <message>
+        <location filename="../bgt_import_dialog_base.ui" line="142"/>
+        <source>Add imported files to project</source>
+        <translation>Voeg geimporteerde bestanden toe</translation>
+    </message>
+    <message>
+        <location filename="../bgt_import_dialog_base.ui" line="158"/>
+        <source>Help</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
